389538a60dd70c0f4bffe0acd6aa187650054580
Sylvain Picarle <sylvain.picarle@gmail.com>
2016-09-14 08:13:54 +0200

    Try to get contact groups

--- QKSMS/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.java
+++ QKSMS/src/main/java/com/moez/QKSMS/receiver/SmsReceiver.java
@@ -1,12 +1,22 @@
 package com.moez.QKSMS.receiver;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.util.Log;
 
+import com.moez.QKSMS.common.vcard.ContactOperations;
+import com.moez.QKSMS.data.ContactHelper;
+import com.moez.QKSMS.data.ContactList;
 import com.moez.QKSMS.data.Conversation;
 import com.moez.QKSMS.mmssms.Message;
 import com.moez.QKSMS.mmssms.Transaction;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 
 public class SmsReceiver extends MessagingReceiver {
 
@@ -19,7 +29,26 @@ public class SmsReceiver extends MessagingReceiver {
         long threadId = transaction != null ? conversation.getThreadId() : 0;
         Message message = new Message("Auto reply "+body,address);
 
+        getGroups(context);
         transaction.sendNewMessage(message,threadId);
     }
+
+    public void getGroups(Context context){
+        final String[] GROUP_PROJECTION = new String[] {
+                ContactsContract.Groups._ID, ContactsContract.Groups.TITLE };
+        Cursor gC = context.getContentResolver().query(
+                ContactsContract.Groups.CONTENT_URI, GROUP_PROJECTION,null,null,null);
+        gC.moveToFirst();
+        LinkedHashSet<String> s = new LinkedHashSet<String>();
+        while (!gC.isAfterLast()) {
+            int idcolumn = gC.getColumnIndex(ContactsContract.Groups.TITLE);
+            String Id = gC.getString(idcolumn);
+            s.add(Id);
+            gC.moveToNext();
+        }
+
+        Log.d("getContent", String.valueOf(s.size()));
+    }
+
 }
 
